b92619ff6c222dd6c9f553e13fb7eb4ccb3a812d
Jenifer29b <Jenifer29b@gmail.com>
2024-07-03 19:45:58 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './App.css'
+const App = () => {
+  return (
+    <div className='app'>
+      <header className='app_header'>Header Section</header>
+      <main className='app_main'>
+        <secion className='task_column'>Section1</secion>
+        <secion className='task_column'>Section2</secion>
+        <secion className='task_column'>Section3</secion>
+      </main>
+    
+    
+    </div>
+  )
+}
+
+export default App
